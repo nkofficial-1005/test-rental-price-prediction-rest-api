2d4213cf16d4e3b5813bfaa313f75e11c7f74744
Nikita Kohli <64784160+nkofficial-1005@users.noreply.github.com>
2026-09-19 02:40:43 +0000

    updated "python slim version from 9 to 11" and "backend url in the frontend app"

--- frontend/app.py
+++ frontend/app.py
@@ -3,7 +3,7 @@ import pandas as pd
 import requests
 
 # Base URL of the Flask backend
-BACKEND_URL = "http://backend:7860"
+BACKEND_URL = "http://localhost:7860"
 
 # Set the title of the Streamlit app
 st.title("Airbnb Rental Price Prediction")
